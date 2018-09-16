a3825150d665db8d625755b760fddc7826141c11
Anton Arboleda <aarboleda@fb.com>
2018-09-16 16:52:01 -0700

    Pass tests

--- week_1/percolation.py
+++ week_1/percolation.py
@@ -17,45 +17,44 @@ class Percolation:
     def open(self, row, col):  # None
         """open site (row, col) if it is not open already
         """
-        self.validate(row, col)
-        if row != 1 and self.isOpen(row - 1, col):
-            self.qf.union(
-                self.get_uf_index(row, col), self.get_uf_index(row - 1, col))
-        if row < self.size and self.isOpen(row + 1, col):
-            self.qf.union(
-                self.get_uf_index(row, col), self.get_uf_index(row + 1, col))
-        if col != 1 and self.isOpen(row, col - 1):
-            self.qf.union(
-                self.get_uf_index(row, col), self.get_uf_index(row, col - 1))
-        if col > self.size and self.isOpen(row, col + 1):
-            self.qf.union(
-                self.get_uf_index(row, col), self.get_uf_index(row, col + 1))
+        self.opened[row - 1][col - 1] = True
 
-    def get_uf_index(self, row, col):
+        if (row == 1):
+            self.qf.union(self.get_qf_index(row, col), self.top)
+        elif (row == self.size):
+            self.qf.union(self.get_qf_index(row, col), self.bottom)
+
+        if row != 1 and self.is_open(row - 1, col):
+            self.qf.union(
+                self.get_qf_index(row, col), self.get_qf_index(row - 1, col))
+        if row < self.size and self.is_open(row + 1, col):
+            self.qf.union(
+                self.get_qf_index(row, col), self.get_qf_index(row + 1, col))
+        if col != 1 and self.is_open(row, col - 1):
+            self.qf.union(
+                self.get_qf_index(row, col), self.get_qf_index(row, col - 1))
+        if col < self.size and self.is_open(row, col + 1):
+            self.qf.union(
+                self.get_qf_index(row, col), self.get_qf_index(row, col + 1))
+
+    def get_qf_index(self, row, col):
         return self.size * (row - 1) + col
 
-    def isOpen(self, row, col):  # bool
-        """Is site (row, col) open?
+    def is_open(self, row, col):  # bool
+        """Is site open
         """
+        return self.opened[row - 1][col - 1]
 
-        # return self.grid[row, col].is_blocked
+    def is_full(self, i, j):
+        if (i > 0 and i <= self.size and j > 0 and j <= self.size):
+            return self.qf.connected(self.top, self.get_qf_index(i, j))
+        else:
+            return False
 
-    def isFull():  # bool
-        """is site (row, col) full?
-        """
-        pass
-
-    def number_of_open_sites(self):  # int
-        """number of open sites
-        """
-        # count = 0
-        # [count_sites(row) for row in self.grid]
-        pass
-
-    def percolates():  # bool
+    def percolates(self):  # bool
         """does the system percolate?
         """
-        pass
+        return self.qf.connected(self.top, self.bottom)
 
     def validate(self, row, col):
         """Validate that row/col is in the grid
@@ -64,9 +63,31 @@ class Percolation:
             raise ValueError('Out of bounds array access!')
 
 
-a = Percolation(5)
-print(a.grid)
-"""Weighted Quick Union Algorithm takes steps to avoid tall trees."""
+class RandomValues:
+    def __init__(self, n):
+        values = list(range(1, n + 1))
+        self.sample = []
+        for y in values:
+            self.sample += [[x, y] for x in values]
+
+
+class PercolationTest(unittest.TestCase):
+    def test_percolation(self):
+        n = 5
+        perc = Percolation(n)
+        rv = RandomValues(n).sample
+        print(rv)
+        c = 0
+
+        while (perc.percolates() is False):
+            print(rv[c])
+            perc.open(*rv[c])
+            print(perc.percolates())
+            c += 1
+            print(perc.qf.array)
+
+        print("Pecolates after " + str(c) + " attempts")
+
 
 if __name__ == '__main__':
     unittest.main()
