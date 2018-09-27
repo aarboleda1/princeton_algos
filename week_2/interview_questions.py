c3bd7929c6e23da4a686a4574b1fbd9750bab117
Anton Arboleda <aarboleda@fb.com>
2018-09-27 09:43:33 -0700

    binary

---
+++ week_2/interview_questions.py
@@ -0,0 +1,34 @@
+"""
+Intersection of two sets. Given two arrays arr a[] and arr b[]
+each containing nn distinct 2D points in the plane, design a subquadratic
+algorithm to count the number of points that are contained both in arr a[]
+and arr b[]
+"""
+
+def sort():
+    pass
+
+
+def intersection_two_sets(arr_a, arr_b):
+    pass
+
+
+"""
+Permutation. Given two integer arrays of size nn, design a subquadratic
+algorithm to determine whether one is a permutation of the other.
+That is, do they contain exactly the same entries but, possibly,
+in a different order.
+"""
+
+"""
+Dutch national flag. Given an array of nn buckets, each containing a red,
+white, or blue pebble, sort them by color. The allowed operations are:
+
+swap(i, j)swap(i,j): swap the pebble in bucket ii with the pebble in bucket jj.
+color(i)color(i): determine the color of the pebble in bucket ii.
+The performance requirements are as follows:
+
+At most nn calls to color()color().
+At most nn calls to swap()swap().
+Constant extra space.
+"""
