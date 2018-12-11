d7ccf9ca7c20b9ce7e5ed8e773591872a8e8850b
Anton Arboleda <aarboleda@fb.com>
2018-12-11 09:14:31 -0800

    more back tracking

--- practice/backtracking/all_perms.py
+++ practice/backtracking/all_perms.py
@@ -1,10 +1,6 @@
 import unittest
+
 """Generate all permutations of a set
-
-
-
-
-
 
 
 Solution:
@@ -13,23 +9,26 @@ Use backtracking
 INPUT: "ABC"
 OUTPUT: ['ABC', 'ACB', 'BAC', 'BCA', 'CBA', 'CAB']
 
+ A
+B  C
+
 """
 
 
 def to_string(alist):
-    return ''.join(alist)
+    return "".join(alist)
 
 
 def permute(alist, left, right, result):
     if left == right:  # a check if is a solution
-        return to_string(alist) # process the solution
+        return to_string(alist)  # process the solution
     else:
-        for i in range(left, right + 1):
+        for i in range(left, right):
             # make move
             alist[left], alist[i] = alist[i], alist[left]
             chars = permute(alist, left + 1, right, result)
             if chars is not None:
-                result.append(permute(alist, left + 1, right, result))
+                result.append(chars)
             # unmake move/backtrack
             alist[i], alist[left] = alist[left], alist[i]
 
@@ -38,9 +37,10 @@ class AllPermsTestCase(unittest.TestCase):
     def test_all_perms(self):
         data = ["A", "B", "C"]
         result = []
-        permute(data, 0, len(data) - 1, result)
+        permute(data, 0, len(data), result)
         self.assertEquals(len(result), 6)
         print(result)
+
 
 if __name__ == "__main__":
     unittest.main()
